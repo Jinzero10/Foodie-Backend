a8267d604487fa8bfec0ddfcc250b0bbe937a34c
Joemari Cabingatan <jcabingatan1010@gmail.com>
2024-01-20 13:17:42 +0800

    deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,3 @@
-const allowedOrigins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://192.168.100.8:3000",
-];
+const allowedOrigins = ["https://foodie-sduq.onrender.com"];
 
 module.exports = allowedOrigins;
